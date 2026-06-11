8b8faffa21493e569a56cc191b54743192936756
JonathanOishi <JonathanOishi@users.noreply.github.com>
2026-06-11 02:31:35 +0000

    deploy: 0f3e0792af6b0f7503eed54c53365517114ece10

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2410956947" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1633941488" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
