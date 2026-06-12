77c2c5d266fa5cda282847ca2b812d1ef891bed9
Jonathan Oishi <jonathan_oishi@hotmail.com>
2026-06-11 22:03:22 -0300

    particcles

--- lib/features/view/widgets/particles/particles_back.dart
+++ lib/features/view/widgets/particles/particles_back.dart
@@ -1,12 +1,11 @@
+// particles_back.dart
 import 'package:flutter/material.dart';
 import 'package:jonathan_oishi_portfolio/core/responsive/responsive.dart';
 import 'package:jonathan_oishi_portfolio/features/view/widgets/particles/particle_paint.dart';
 import 'package:jonathan_oishi_portfolio/features/view/widgets/particles/particles.dart';
 
 class ParticleBackground extends StatefulWidget {
-  const ParticleBackground({
-    super.key,
-  });
+  const ParticleBackground({super.key});
 
   @override
   State<ParticleBackground> createState() => _ParticleBackgroundState();
@@ -14,82 +13,65 @@ class ParticleBackground extends StatefulWidget {
 
 class _ParticleBackgroundState extends State<ParticleBackground>
     with SingleTickerProviderStateMixin {
-  late AnimationController controller;
-
-  List<Particle> particles = [];
+  late AnimationController _controller;
+  late List<Particle> _particles;
+  late _ParticleNotifier _notifier;
 
   @override
   void initState() {
     super.initState();
+    _particles = [];
+    _notifier = _ParticleNotifier();
 
-    controller =
-        AnimationController(
-            vsync: this,
-            duration: const Duration(
-              seconds: 60,
-            ),
-          )
-          ..addListener(updateParticles)
-          ..repeat();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 60),
+    )..repeat();
+
+    _controller.addListener(_updateParticles);
   }
 
-  void updateParticles() {
-    final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
+  void _updateParticles() {
+    if (!mounted) return;
+    final size = MediaQuery.of(context).size;
 
-    for (final particle in particles) {
-      particle.x += particle.velocityX;
-      particle.y += particle.velocityY;
-
-      if (particle.x < 0) {
-        particle.x = screenWidth;
-      }
-
-      if (particle.x > screenWidth) {
-        particle.x = 0;
-      }
-
-      if (particle.y < 0) {
-        particle.y = screenHeight;
-      }
-
-      if (particle.y > screenHeight) {
-        particle.y = 0;
-      }
+    for (final p in _particles) {
+      p.x += p.velocityX;
+      p.y += p.velocityY;
+      if (p.x < 0) p.x = size.width;
+      if (p.x > size.width) p.x = 0;
+      if (p.y < 0) p.y = size.height;
+      if (p.y > size.height) p.y = 0;
     }
 
-    setState(() {});
+    _notifier.notify();
   }
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (context, constraints) {
-        if (particles.isEmpty) {
-          final particleCount = Responsive.responsive<int>(
+        if (_particles.isEmpty) {
+          final count = Responsive.responsive<int>(
             context: context,
             mobile: 20,
             tablet: 50,
             desktop: 80,
           );
-
-          particles = List.generate(
-            particleCount,
-            (_) => Particle.random(
-              constraints.maxWidth,
-              constraints.maxHeight,
-            ),
+          _particles = List.generate(
+            count,
+            (_) => Particle.random(constraints.maxWidth, constraints.maxHeight),
           );
         }
 
-        return CustomPaint(
-          size: Size(
-            constraints.maxWidth,
-            constraints.maxHeight,
-          ),
-          painter: ParticlePainter(
-            particles: particles,
-          ),
+        return AnimatedBuilder(
+          animation: _notifier,
+          builder: (context, _) {
+            return CustomPaint(
+              size: Size(constraints.maxWidth, constraints.maxHeight),
+              painter: ParticlePainter(particles: _particles),
+            );
+          },
         );
       },
     );
@@ -97,7 +79,12 @@ class _ParticleBackgroundState extends State<ParticleBackground>
 
   @override
   void dispose() {
-    controller.dispose();
+    _controller.dispose();
+    _notifier.dispose();
     super.dispose();
   }
+}
+
+class _ParticleNotifier extends ChangeNotifier {
+  void notify() => notifyListeners();
 }
